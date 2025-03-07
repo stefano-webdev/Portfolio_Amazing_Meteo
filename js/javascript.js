8c90d0b05a20187a1a557a9b170c65258c7d341a
Stefano Quaranta <stefanooquaranta@gmail.com>
2025-03-07 22:24:50 +0100

    Primo commit, home page fatta. (lato mobile)

---
+++ js/javascript.js
@@ -0,0 +1,38 @@
+// 1 Attiva il toggle dark_light
+function attiva_toggle_dark_light() {
+    if (root.classList.contains("light_mode")) {
+        // 1.1 Attiva e salva dark mode
+        root.classList.remove("light_mode");
+        root.classList.add("dark_mode");
+        localStorage.setItem("mode", "dark");
+    }
+    else {
+        // 1.2 Attiva e salva light mode
+        root.classList.remove("dark_mode");
+        root.classList.add("light_mode");
+        localStorage.setItem("mode", "light");
+    }
+}
+
+// 2 Apertura menu mobile
+const btn_menu_toggle = document.getElementById("btn_menu_toggle");
+const menu = document.getElementById("menu");
+btn_menu_toggle.addEventListener("click", () => {
+    btn_menu_toggle.classList.toggle("open");
+    menu.classList.toggle("open");
+});
+
+// 3 Switch light/dark mode
+const btn_light_dark = document.getElementById("btn_light_dark_toggle");
+const root = document.documentElement;
+const theme = localStorage.getItem("mode");
+
+// 3.1 Se theme non è null e theme è dark, applico la dark mode
+if (!(theme === null) && theme != "light") {
+    root.classList.remove("light_mode");
+    root.classList.add("dark_mode");
+}
+
+// 3.2 Aggiunge l'evento click al toggle e
+// l'interattività con toggle per l'accessibilità 
+btn_light_dark.addEventListener("click", attiva_toggle_dark_light)
