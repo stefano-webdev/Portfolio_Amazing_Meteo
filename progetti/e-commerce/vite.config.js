317ec77f9d91de625e1b1ca5adbf06b42b82ff68
Stefano Quaranta <stefanooquaranta@gmail.com>
2025-06-17 14:28:56 +0200

    Fix per vite.config.js

--- progetti/e-commerce/vite.config.js
+++ progetti/e-commerce/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/e-commerce/',
+  base: '/progetti/e-commerce/',
   plugins: [react()],
 })
